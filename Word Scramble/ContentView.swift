36bb4b0464a3e9262fae50fe2ab23c1259a2b9c4
Soumyattam Dey <soumyttamdey14@gmail.com>
2021-06-29 17:42:19 +0530

    Adding comments

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var usedWords=[String]()
+    @State private var usedWords=[String]()     //stores players words
     @State private var newWord=""
     @State private var rootWord=""
+    
     @State private var isNewGame=false
     @State private var score=0
     
@@ -32,23 +33,23 @@ struct ContentView: View {
                     .font(.title3)
                     .fontWeight(.medium)
                     
-                    
-                
+                //View to show player's words
                 List(usedWords,id:\.self){
                     Image(systemName: "\($0.count).circle")
                     Text($0)
                 }
             }
-            .navigationBarTitle(rootWord)
+            .navigationBarTitle(rootWord)           //Display a random word
             .navigationBarItems(trailing: Button(action: startGame, label: {
                 isNewGame ? Text("New word") : Text("Start Game")
-            }))
+            }))             //Start a new game
             .alert(isPresented: $showingError){
                 Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("Ok")))
             }
         }
     }
     
+    //called to start a new game
     func startGame(){
         usedWords.removeAll()
         score=0
@@ -61,47 +62,55 @@ struct ContentView: View {
             }
         }
         
+        //if there is any sort of error the app close automatically
         fatalError("Could not load start.txt from bundle")
     }
     
+    //called with onCommit()->when a new word is entered
     func addNewWord(){
         let answer=newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        
+        //checks if it is an empty string or not
         guard answer.count>0 else {
             return
         }
         
+        //check if it is original that is not entered earlier
         guard isOriginal(word: answer) else {
             wordError(title: "Word already used", message: "Be more original")
             return
         }
         
+        //check if it is a two letter word
         guard twoLetterWords(word: answer) else {
             wordError(title: "Not allowed", message: "Words must be more than two letters")
             return
         }
         
+        //check if the word is possible to make fromm the root word
         guard isPossible(word: answer) else {
             wordError(title: "Word not recognzed", message: "You can't just make them up, you know !")
             return
         }
         
+        //check if the word is correct (calling UITextChecker())
         guard isReal(word: answer) else {
             wordError(title: "Word not possible", message: "That isn't a real world")
             return
         }
         
-        usedWords.insert(answer, at: 0)
+        usedWords.insert(answer, at: 0)             //insert word in the array
         newWord=""
-        score=score+answer.count+usedWords.count
+        score=score+answer.count+usedWords.count    //update user score
     }
     
     func isOriginal(word:String)->Bool{
+        
+        //if the word is the root word then return false
         if word==rootWord{
             return false
         }
-        return !usedWords.contains(word)
+        return !usedWords.contains(word)    //checking if it is already in the array
         
     }
     
